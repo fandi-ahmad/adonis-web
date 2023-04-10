652b792cdc70db84f9d338ba7595b7e91b62aa6e
fandi-ahmad <fandi4160@gmail.com>
2023-04-10 23:21:24 +0800

    add delete

--- start/routes.ts
+++ start/routes.ts
@@ -37,22 +37,13 @@ Route.get('/news', 'ArticlesController.index').as('news_view')
 Route.get('/news/create', 'ArticlesController.create').as('news_create')
 Route.post('/news/create', 'ArticlesController.store').as('news_store')
 Route.get('/news/:slug/edit', 'ArticlesController.edit').as('news_edit')
-
-
 Route.patch('/news/:slug', 'ArticlesController.update').as('news_update')
+Route.delete('/news/:slug', 'ArticlesController.destroy').as('news_delete')
 
 // .where('id', {
 //   match: /^[0-9]+$/,
 //   cast: (id) => Number(id),
 // }).as('news_update')
-
-Route.delete('/news/:id', ({params}) => {
-  return { params }
-}).where('id', {
-  match: /^[0-9]+$/,
-  cast: (id) => Number(id),
-})
-
 
 // ==== penambahan kode ini untuk merubah id menjadi integer ====
 // .where('id', {
